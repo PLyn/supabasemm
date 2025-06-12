ed3fc769b9e0d299b3998cbe495ef67fb965c6fa
Peter <plyn8750@gmail.com>
2025-06-12 08:50:40 -0500

    Fix layouts to avoid layout shifting (#4)

--- src/migrate/components/results_view.rs
+++ src/migrate/components/results_view.rs
@@ -8,35 +8,42 @@ pub fn ResultsView(
     dest_heading: String,
 ) -> impl IntoView {
     view! {
-        <div class="w-full max-w-8xl overflow-x-auto">
-            <table class="table w-full border-collapse border border-black">
-                <thead>
-                    <tr>
-                        <th class="p-2 text-center bg-gray-300 border border-black">"Service"</th>
-                        <th class="p-2 text-center bg-gray-300 border border-black">"Config Item"</th>
-                        <th class="p-2 text-center bg-gray-300 border border-black">{source_heading}</th>
-                        <th class="p-2 text-center bg-gray-300 border border-black">{dest_heading}</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <For
-                        each=move || results_rw.get().into_iter()
-                        key=|project_config| project_config.name.clone()
-                        children=move |project_config| {
-                            project_config.diffs.iter().map(|diff| {
-                                view! {
-                                    <tr class="hover:bg-gray-200">
-                                        <td class="p-2 text-left border border-black">{project_config.name.clone()}</td>
-                                        <td class="p-2 text-left border border-black">{diff.key.clone()}</td>
-                                        <td class="p-2 border border-black">{diff.source_value.clone()}</td>
-                                        <td class="p-2 border border-black">{diff.dest_value.clone()}</td>
+        <div class="w-full max-w-8xl space-y-6">
+            <For
+                each=move || results_rw.get().into_iter()
+                key=|project_config| project_config.name.clone()
+                children=move |project_config| {
+                    view! {
+                        <div class="overflow-x-auto">
+                            <h2 class="text-lg font-bold mb-2">{project_config.name.clone()}</h2>
+                            <table class="table w-full border-collapse border border-black">
+                                <thead>
+                                    <tr>
+                                        <th class="p-2 text-center bg-gray-300 border border-black">"Config Item"</th>
+                                        <th class="p-2 text-center bg-gray-300 border border-black">{source_heading.clone()}</th>
+                                        <th class="p-2 text-center bg-gray-300 border border-black">{dest_heading.clone()}</th>
                                     </tr>
-                                }
-                            }).collect::<Vec<_>>().into_view()
-                        }
-                    />
-                </tbody>
-            </table>
+                                </thead>
+                                <tbody>
+                                    <For
+                                        each=move || project_config.diffs.clone().into_iter()
+                                        key=|diff| diff.key.clone()
+                                        children=move |diff| {
+                                            view! {
+                                                <tr class="hover:bg-gray-200">
+                                                    <td class="p-2 text-left border border-black">{diff.key}</td>
+                                                    <td class="p-2 border border-black">{diff.source_value}</td>
+                                                    <td class="p-2 border border-black">{diff.dest_value}</td>
+                                                </tr>
+                                            }
+                                        }
+                                    />
+                                </tbody>
+                            </table>
+                        </div>
+                    }
+                }
+            />
         </div>
     }
 }
